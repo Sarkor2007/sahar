47b94922b78a115f596c39cc41be20dc9b7a8df2
SArkor <abilkosimovsarkor@gmail.com>
2023-10-09 19:00:12 +0500

    feat: add team, about and features sections

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,12 @@ const langMenu = document.querySelector('.header__lang-list');
 langBtn.addEventListener('click', () => {
     langMenu.classList.toggle('active');
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
