0af27a6a2c83039171304e1344dc7cd0e91059d6
SArkor <abilkosimovsarkor@gmail.com>
2023-10-09 21:11:18 +0500

    feat: add questions section

--- scripts/app.js
+++ scripts/app.js
@@ -10,8 +10,33 @@ langBtn.addEventListener('click', () => {
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
+    loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+var swiper = new Swiper(".mySwiperTwo", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+});
+
+
+const accordionBtn = document.querySelectorAll('.questions__item-btn');
+const accordionBody = document.querySelectorAll('.questions__item-content');
+const accordionArrow = document.querySelectorAll('.questions__item-btn img');
+
+for (let i = 0; i < accordionBtn.length; i++) {
+    accordionBtn[i]?.addEventListener('click', () => {
+        accordionBody[i].classList.toggle('active');
+        accordionArrow[i].classList.toggle('active');
+        if (accordionBody[i].classList.contains('active')) {
+            accordionBody[i].style.minHeight = accordionBody[i].scrollHeight + 'px';
+        } else {
+            accordionBody[i].style.minHeight = '0';
+        }
+    })
+}
