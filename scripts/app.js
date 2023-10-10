ab59391b6672d4b6929a38d8c321df1e059bdc5e
SArkor <abilkosimovsarkor@gmail.com>
2023-10-10 20:22:06 +0500

    feat: add responsive for header

--- scripts/app.js
+++ scripts/app.js
@@ -42,3 +42,27 @@ for (let i = 0; i < accordionBtn.length; i++) {
         }
     })
 }
+
+const burgerOpen = document.querySelector('.nav__open');
+const burgerExit = document.querySelector('.nav__exit-btn');
+const nav = document.querySelector('.nav');
+const body = document.querySelector('body')
+const filter = document.querySelector('.filter')
+
+burgerOpen.addEventListener('click', () => {
+    nav.classList.add('active');
+    body.classList.add('active');
+    filter.classList.add('active');
+})
+
+burgerExit.addEventListener('click', () => {
+    nav.classList.remove('active');
+    body.classList.remove('active');
+    filter.classList.remove('active');
+})
+
+filter.addEventListener('click', () => {
+    nav.classList.remove('active');
+    body.classList.remove('active');
+    filter.classList.remove('active');
+})
